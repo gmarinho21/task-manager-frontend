529c540e71881737e4325737f65a1ba7e112493c
gmarinho21 <gabriel.jaka@gmail.com>
2023-11-23 19:40:53 -0400

    Deleted duplicates

--- @/store/isUserLogged.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useIsUserLoggedStore = void 0;
-var zustand_1 = require("zustand");
-exports.useIsUserLoggedStore = (0, zustand_1.create)(function (set) { return ({
-    isLogged: false,
-    changeLoggedState: function (newState) { return set(function () { return ({ isLogged: newState }); }); },
-}); });
--- @/store/loggedUser.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useUserLoggedStore = void 0;
-var zustand_1 = require("zustand");
-exports.useUserLoggedStore = (0, zustand_1.create)(function (set) { return ({
-    userLogged: {},
-    changeUser: function (newUser) { return set(function () { return ({ userLogged: newUser }); }); },
-}); });
